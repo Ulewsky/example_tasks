4838003f9ca10e7334d29478e7c4385282a4ad6e
Maciej Ulewski <maciejulewski@macbook-pro-maciej.home>
2020-10-06 19:13:12 +0200

    Created binary tree and searching for  pesel number

--- task_1.py
+++ task_1.py
@@ -20,8 +20,63 @@ with open(args.filename) as json_file:
     for p in dat_imp['people']:
         person.append(Graph(p['first_name'],(p['last_name']),p['pesel']))
         
-
 node = []
+pesels = []
+answer = []
 for node in person:
-    print(node)
-    
+    pesels.append(int(node.pesel))
+    answer.append([node.first_name,node.last_name,node.pesel])
+
+print("List of persons in the JSON file: ",answer)#Elements of the file
+#Creating binary tree
+class Tree:
+    def __init__(self, val):
+        self.l = None
+        self.r = None
+        self.val = val
+    def insert(self, val):
+        if val < self.val:
+            if self.l is None:
+                self.l = Tree(val)
+            else:
+                self.l.insert(val)
+        elif val > self.val:
+            if self.r is None:
+                self.r = Tree(val)
+            else:
+                self.r.insert(val)
+            
+    def build_tree(self):
+        if self.l:
+            self.l.build_tree()
+        print(self.val)
+        if self.r:
+            self.r.build_tree()
+
+root =[]
+root = Tree(pesels[0])
+
+for i in range(1,len(pesels)):
+    root.insert(pesels[i])
+#Finding person with exact pesel number    
+answer2 = []
+z = []
+m = 0
+root.build_tree()
+x = input("Do you want to find someone? ")
+if x == 'No':
+    print("Ok, thanks ")
+elif x =='Yes':
+    number = int(input("Write pesel number of that person "))
+    for z in person:
+        if number == int(z.pesel):
+            m = 1
+            answer2.append([z.first_name,z.last_name,z.pesel])
+            break
+    if m == 1:
+        print(answer2)
+    elif m == 0:
+        print('Pesel {} not found'.format(number))
+            
+else:
+    print('Wrong answer, write Yes or No')
